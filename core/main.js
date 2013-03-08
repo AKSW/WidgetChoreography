4cf624a22cd774bebfcb91e58af42276205fe8b7
Clemens Hoffmann <cannelony@googlemail.com>
2013-03-08 18:53:41 +0100

    add functionality on settings buttons and mockup of literal widget

--- core/main.js
+++ core/main.js
@@ -22,39 +22,39 @@ $(document).ready(function() {
 
 
   // markItUp settings
-var markItUpSettings = {
+  var markItUpSettings = {
     nameSpace: 'markdown', // Useful to prevent multi-instances CSS conflict
     onShiftEnter: {keepDefault:false, openWith:'\n\n'},
     returnParserData: function(data){
-        //added new callback in markItUp to use json markdown parser (in this case: showdown)
-        return self._converter.makeHtml(data);
+      //added new callback in markItUp to use json markdown parser (in this case: showdown)
+      return self._converter.makeHtml(data);
     },
     markupSet: [
-        // {name:'First Level Heading', key:"1", placeHolder:'Your title here...',
-         // closeWith:function(markItUp) { return self._miu.markdownTitle(markItUp, '=') } },
-        // {name:'Second Level Heading', key:"2", placeHolder:'Your title here...',
-         // closeWith:function(markItUp) { return self._miu.markdownTitle(markItUp, '-') } },
-        // {name:'Heading 3', key:"3", openWith:'### ', placeHolder:'Your title here...' },
-        // {name:'Heading 4', key:"4", openWith:'#### ', placeHolder:'Your title here...' },
-        // {name:'Heading 5', key:"5", openWith:'##### ', placeHolder:'Your title here...' },
-        // {name:'Heading 6', key:"6", openWith:'###### ', placeHolder:'Your title here...' },
-        // {separator:'---------------' },
-        {name:'Bold', key:"B", openWith:'**', closeWith:'**'},
-        {name:'Italic', key:"I", openWith:'_', closeWith:'_'},
-        {separator:'---------------' },
-        {name:'Bulleted List', openWith:'- ' },
-        {name:'Numeric List', openWith:function(markItUp) {
-          return markItUp.line+'. ';
-        }},
-        {separator:'---------------' },
-        {name:'Picture', key:"P", replaceWith:'![[![Alternative text]!]]([![Url:!:http://]!] "[![Title]!]")'},
-        {name:'Link', key:"L", openWith:'[', closeWith:']([![Url:!:http://]!] "[![Title]!]")',
-         placeHolder:'Your text to link here...' },
-        // {separator:'---------------'},
-        // {name:'Quotes', openWith:'> '},
-        // {name:'Code Block / Code', openWith:'(!(\t|!|`)!)', closeWith:'(!(`)!)'},
-        {separator:'---------------'},
-        {name:'Preview', call:'preview', className:"preview"}
+      // {name:'First Level Heading', key:"1", placeHolder:'Your title here...',
+      // closeWith:function(markItUp) { return self._miu.markdownTitle(markItUp, '=') } },
+      // {name:'Second Level Heading', key:"2", placeHolder:'Your title here...',
+      // closeWith:function(markItUp) { return self._miu.markdownTitle(markItUp, '-') } },
+      // {name:'Heading 3', key:"3", openWith:'### ', placeHolder:'Your title here...' },
+      // {name:'Heading 4', key:"4", openWith:'#### ', placeHolder:'Your title here...' },
+      // {name:'Heading 5', key:"5", openWith:'##### ', placeHolder:'Your title here...' },
+      // {name:'Heading 6', key:"6", openWith:'###### ', placeHolder:'Your title here...' },
+      // {separator:'---------------' },
+      {name:'Bold', key:"B", openWith:'**', closeWith:'**'},
+      {name:'Italic', key:"I", openWith:'_', closeWith:'_'},
+      {separator:'---------------' },
+      {name:'Bulleted List', openWith:'- ' },
+      {name:'Numeric List', openWith:function(markItUp) {
+        return markItUp.line+'. ';
+      }},
+      {separator:'---------------' },
+      {name:'Picture', key:"P", replaceWith:'![[![Alternative text]!]]([![Url:!:http://]!] "[![Title]!]")'},
+      {name:'Link', key:"L", openWith:'[', closeWith:']([![Url:!:http://]!] "[![Title]!]")',
+       placeHolder:'Your text to link here...' },
+      // {separator:'---------------'},
+      // {name:'Quotes', openWith:'> '},
+      // {name:'Code Block / Code', openWith:'(!(\t|!|`)!)', closeWith:'(!(`)!)'},
+      {separator:'---------------'},
+      {name:'Preview', call:'preview', className:"preview"}
     ]
   };
 
@@ -67,10 +67,40 @@ var markItUpSettings = {
     // .end()
     // .find( ".portlet-content" );
 
-  $('.portlet .actionbar .icon-arrow-up').click(function(event) {
+  $(document).on('click', '.portlet .dropdown-menu li a', function(event) {
     event.preventDefault();
-    $(this).toggleClass('icon-arrow-up').toggleClass('icon-arrow-down');
-    $(this).parents('.portlet:first').toggleClass('portlet-minimized');
+    var portlet = $(this).parents('.portlet');
+    // add property
+    if ($(this).hasClass('add')) {
+      var markup = '<div data-content="foo:bar" class="portlet-entry">\
+                      <div class="control-group">\
+                        <!-- <label class="control-label">foo:bar</label> -->\
+                        <div class="controls">\
+                          <div class="line input-prepend input-append">\
+                            <span class="add-on">\
+                              <i class="icon-bookmark"></i>\
+                            </span><input type="text" value="Add Foo Bar Test" class="input resource-widget"><button class="btn" type="button"><i class="icon-cog"></i></button>\
+                          </div>\
+                        </div>\
+                      </div>\
+                    </div>';
+      portlet.find('.portlet-content').append(markup);
+    }
+
+    // remove portlet
+    if ($(this).hasClass('remove')) {
+      portlet.fadeOut(400, function() {
+        $(this).remove();
+      });
+    }
+
+    // hide - show
+    if ($(this).hasClass('hide-show')) {
+      $(this).parents('.dropdown-menu').css('z-index', '1500');
+      $(this).find('i').toggleClass('icon-arrow-up').toggleClass('icon-arrow-down');
+      portlet.toggleClass('portlet-minimized');
+    }
+
   });
 
   $('.portlet .actionbar .icon-trash').click(function(event) {
